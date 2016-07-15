e15a53dd2bbb1a1a8f6d1a64f06499aefeaae31e
yeguanghao <yeguanghao1999@gmail.com>
2016-07-15 13:31:23 +0800

    update

--- note/cg.cc
+++ note/cg.cc
@@ -26,6 +26,11 @@ typedef long long ll;
 typedef unsigned long long ull;
 typedef pair<int,int> pii;
 typedef pair<ll,ll> pll;
+const int Eps=1e-7;
+int Sign(double x) {
+	if(fabs(x)<Eps) return 0;
+	return x>0?1:-1;
+}
 struct Point{
 	double x,y;
 	Point(double _x=0,double _y=0):x(_x),y(_y){}
@@ -40,6 +45,9 @@ struct Point{
 	}
 	double operator * (const Point &b) const {
 		return x*b.x+y*b.y;
+	}
+	bool operator == (const Point &b) const {
+		return (Sign(x-b.x)==0)&&(Sign(y-b.y)==0);
 	}
 };
 bool toLeft(Point a, Point b, Point x) {
@@ -79,6 +87,68 @@ vector<Point> extremePoint(int size,Point p[]) {
 	origin=Point(0,0);
 	return ret;
 }
+vector<Point> extremeEdge(int size,Point p[]) {
+	bool *isExtreme=new bool[size+10];
+	memset(isExtreme,0,sizeof(bool)*(size+10));
+	for(int i=1;i<=n;++i) {
+		for(int j=i+1;j<=n;++j) {
+			if(p[i]==p[j]) continue;
+			bool lEmpty=1,rEmpty=1;
+			for(int k=1;k<=n;++k) {
+				if(k==i||k==j) continue;
+				if(toLeft(p[i],p[j],p[k])) {
+					lEmpty=0;
+				} else {
+					rEmpty=0;
+				}
+			}
+			if(lEmpty^rEmpty) {
+				isExtreme[i]=1; isExtreme[j]=1;
+			}
+		}
+	}
+	vector<Point> ret;
+	for(int i=1;i<=size;++i) {
+		if(isExtreme[i]) ret.pb(p[i]);
+		else origin=p[i];
+	}
+	sort(ret.begin(),ret.end(),cmp);	
+	origin=Point(0,0);
+	return ret;
+}
+vector<Point> incrementalConstruct(int size,Point p[]) {
+	vector<Point> ret;
+	for(int i=1;i<=n;++i) {
+		if(ret.size()<2) {
+			ret.pb(p[i]);
+			continue;
+		}
+		vector<int> pos(2);
+		bool Flag=0;
+		for(int j=0;j<ret.size();++j) {
+			int succ=(j+1)%ret.size(),prev= (j-1 +ret.size())%ret.size();
+			if(toLeft(p[i],ret[j],ret[succ])==toLeft(p[i],ret[j],ret[prev])) {
+				if(toLeft(p[i],ret[j],ret[succ])) {
+					pos[1]=j;
+					Flag=1;
+				} else {
+					pos[0]=j;
+					Flag=1;
+				}
+			}
+		}
+		if(!Flag) continue;
+		vector<Point> temp;
+		temp.pb(p[i]);
+		for(int j=pos[0];;++j) {
+			if(j>=ret.size()) j-=ret.size();
+			temp.pb(ret[j]);
+			if(j==pos[1]) break;
+		}
+		ret=temp;
+	}
+	return ret;
+}
 double Circum(vector<Point> p) {
 	double ret=0;
 	for(int i=1;i<p.size();++i) {
@@ -94,7 +164,8 @@ int main() {
 	for(int i=1;i<=n;++i) {
 		scanf("%lf%lf",&point[i].x,&point[i].y);
 	}
-	double ans=Circum(extremePoint(n,point));
-	printf("%.1f\n",ans);
+	n=unique(point+1,point+n+1)-point-1;
+	double ans=Circum(incrementalConstruct(n,point));
+	printf("%.1lf\n",ans);
 }
 
